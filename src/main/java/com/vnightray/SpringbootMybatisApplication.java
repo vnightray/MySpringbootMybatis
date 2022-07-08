96a36bdcda000577f22c3a9698dce5f8447d6b3f
vnightray <vessalius_oz@163.com>
2022-07-08 20:38:28 +0800

    springboot + mybatis

---
+++ src/main/java/com/vnightray/SpringbootMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.vnightray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+//@ConfigurationProperties("application.properties")
+public class SpringbootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisApplication.class, args);
+	}
+}
---
+++ src/main/java/com/vnightray/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.vnightray.mapper;
+
+import com.vnightray.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    public List<User> queryUserList();
+
+}
